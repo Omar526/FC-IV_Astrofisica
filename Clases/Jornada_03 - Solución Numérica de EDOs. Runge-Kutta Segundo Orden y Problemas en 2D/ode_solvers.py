e2c739421ccb756b9d7ba7a5fca3b0916b68350c
Omar Fernández <104533027+Omar526@users.noreply.github.com>
2025-09-03 19:43:09 -0400

    Add files via upload

---
+++ Clases/Jornada_03 - Solución Numérica de EDOs. Runge-Kutta Segundo Orden y Problemas en 2D/ode_solvers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on [date] [time]
+
+@author: []
+@mail: []
+
+Description: Módulo de solucionadores de EDOs
+"""
+
+# importamos librerías
+import numpy as np
+import matplotlib.pyplot as plt
+
+from typing import Callable, Union, Tuple
+
+# definición de métodos numéricos para resolver EDOs
+def euler_method_second_order_2D():
+    pass
+
+def rk2_method_second_order_2D():
+    pass
+
+def rk4_method_second_order_2D():
+    pass
